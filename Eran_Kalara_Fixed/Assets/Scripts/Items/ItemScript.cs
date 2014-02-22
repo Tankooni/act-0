9b039bab8126c8741d8a5c4af14e3ebd80c9fbd8
Linden Krouse <Linden Krouse@localhost>
2014-02-22 12:17:19 +0000

    Initial commit

---
+++ Eran_Kalara_Fixed/Assets/Scripts/Items/ItemScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemScript : MonoBehaviour 
+{
+    public AdvancedAttributes AdvAttributeBonuses;
+    //Insert "Given Abilities"
+    public eItemTypes ItemType;
+}
