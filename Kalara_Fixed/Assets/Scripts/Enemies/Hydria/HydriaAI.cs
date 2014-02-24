add38f056ed691c5732833faa5cd9e814f6a549a
Linden Krouse <ztaticnull@gmail.com>
2014-02-24 14:55:53 -0500

    Added Aggresive AI stuff. Does not actually work yet.

--- Kalara_Fixed/Assets/Scripts/Enemies/Hydria/HydriaAI.cs
+++ Kalara_Fixed/Assets/Scripts/Enemies/Hydria/HydriaAI.cs
@@ -32,8 +32,22 @@ public class HydriaAI : BaseAI
     [System.Serializable]
     public class StateAggressive : IState {
         private HydriaAI parent;
-        
+
+        private StateMachine currentTask = new StateMachine();
+
+        //circle state variables. Must be here to edit in unity and not cause errors
+        public float circleRadius;
+        public float runSpeed;
+        public float avgRunTime;
+        public float runDeviation;
+
         public StateAggressive(){
+            StateCircle sCircle = new StateCircle();
+            sCircle.setParent(this);
+            
+            currentTask.AddState("circle", sCircle);
+            
+            currentTask.ChangeState("circle");
         }
 
         public void setParent(HydriaAI parent) {
@@ -41,7 +55,48 @@ public class HydriaAI : BaseAI
         }
         
         void IState.UpdateState(){
-        
+            currentTask.Update();
+        }
+
+        [System.Serializable]
+        public class StateCircle : IState {
+            private StateAggressive parent;
+
+            public void setParent(StateAggressive parent) {
+                this.parent = parent;
+            }
+
+            void IState.UpdateState() {
+                //if player gets to close, attack them immediately
+
+                Vector3 toPlayer = parent.parent.transform.position - parent.parent.player.transform.position;
+
+                //don't turn left, always circle to the right
+                float turnRate = (parent.runSpeed / parent.circleRadius) * Time.deltaTime;
+
+                Debug.Log(turnRate);
+
+                float turnScaleFactor = toPlayer.magnitude / parent.circleRadius;
+                turnRate *= turnScaleFactor;
+
+                Debug.Log(turnRate);
+
+                //find the unit normal to the vector pointing to the player and turn based on
+                //the above turn rate. Then give it our movespeed and...well move there
+                Vector3 movementVec = MathEx.rotateXZ(Vector3.Normalize(toPlayer), turnRate) * parent.runSpeed;
+
+                Debug.Log(movementVec);
+
+                parent.parent.transform.position += movementVec;
+            }
+        }
+
+        public class StateLunge {
+            
+        }
+
+        public class StateBackoff {
+
         }
     }
 
