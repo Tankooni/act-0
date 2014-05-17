84080dc98c61833320f269e38cc1409f06dcfad3
Tankooni <Tankooni@localhost>
2014-05-17 19:49:37 -0400

    Yes.

---
+++ Kalara_Fixed/Assets/Scripts/TestBlob/BlobMovementStateMachine.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlobMovementStateMachine : MonoBehaviour
+{
+    public bool IsActive = true;
+    public Transform ActivePrfab;
+    Transform MyTransform;
+
+	IEnumerator Start()
+    {
+        MyTransform = transform;
+        while(true)
+        {
+            yield return new WaitForSeconds(.1f);
+            if(IsActive)
+            {
+                Transform spawnedObject = (Transform)Instantiate(ActivePrfab, MyTransform.position + MyTransform.up, MyTransform.rotation);
+                spawnedObject.rigidbody.AddForce(MyTransform.up * Random.value * 10f);
+            }
+        }
+	}
+	
+	void Update()
+    {
+	
+	}
+}
---
+++ Kalara_Fixed/Assets/Scripts/TestBlob/Bubbles/RiseAndDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RiseAndDestroy : MonoBehaviour
+{
+    public float LiveTime = 2f;
+    Transform _transform;
+
+	// Use this for initialization
+	void Start()
+    {
+        _transform = transform;
+        //Destroy(gameObject, LiveTime);
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        //_transform.position += Vector3.up * Time.deltaTime;
+	}
+}
