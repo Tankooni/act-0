e8130b035b50fee4469e5372e84e112b28e2260d
Zack Wheeler <913966+Tankooni@users.noreply.github.com>
2014-05-18 00:03:39 -0400

    Stuff

--- Kalara_Fixed/Assets/Scripts/TestBlob/Bubbles/RiseAndDestroy.cs
+++ Kalara_Fixed/Assets/Scripts/TestBlob/Bubbles/RiseAndDestroy.cs
@@ -5,17 +5,22 @@ public class RiseAndDestroy : MonoBehaviour
 {
     public float LiveTime = 2f;
     Transform _transform;
+    Vector3 _origLocScale;
+    float StartTime;
 
 	// Use this for initialization
 	void Start()
     {
         _transform = transform;
+        _origLocScale = transform.localScale;
         //Destroy(gameObject, LiveTime);
+        StartTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update()
     {
         //_transform.position += Vector3.up * Time.deltaTime;
+        //_transform.localScale = new Vector3(_origLocScale.x * (Mathf.Sin(Time.time - StartTime)+1), _origLocScale.x * (Mathf.Sin(Time.time - StartTime)+1), _origLocScale.x * (Mathf.Sin(Time.time - StartTime)+1));
 	}
 }
