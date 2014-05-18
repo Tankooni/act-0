7a5cbdbd0b411215f17799b45644867a52553aa2
Zack Wheeler <913966+Tankooni@users.noreply.github.com>
2014-05-18 17:56:00 -0400

    Monster

---
+++ Kalara_Fixed/Assets/Scripts/TestScripts/AdjustForwardToPointToCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustForwardToPointToCamera : MonoBehaviour
+{
+    Transform _transform;
+    Transform _cameraTransform;
+
+	void Start()
+    {
+        _transform = transform;
+        _cameraTransform = Camera.main.transform;
+	}
+	
+	void Update()
+    {
+        _transform.rotation = Quaternion.FromToRotation(_transform.forward, _cameraTransform.forward) * _transform.rotation;
+	}
+}
