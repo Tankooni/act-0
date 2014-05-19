eac8cae2249c45b571673b8b4cc8c46a289ef852
Linden Krouse <ztaticnull@gmail.com>
2014-05-19 17:10:19 -0400

    Camera and movement more or less work.

--- Kalara_Fixed/Assets/Scripts/PlayerScript.cs
+++ Kalara_Fixed/Assets/Scripts/PlayerScript.cs
@@ -63,17 +63,7 @@ public class PlayerScript : MonoBehaviour
     /// <param name="direction">Direction.</param>
     void ApplyHorizontalMovement(Vector3 direction)
     {
-        if (direction.sqrMagnitude > 1)
-        {
-            direction.Normalize();
-            //Debug.Log("Normalized dir");
-        }
-        //Debug.Log("dir1: " + direction);
-
-        //cc.transform.rotation = Quaternion.FromToRotation(transform.forward, Vector3.Scale(playerCamera.transform.right, direction));
-        cc.Move((direction.z * playerCamera.transform.forward) * stats.ACTUAL_STATS.Speed * Time.deltaTime);
-
-        //cc.Move((direction.x * playerCamera.transform.right) * stats.ACTUAL_STATS.Speed * Time.deltaTime);
+        cc.Move(direction * stats.ACTUAL_STATS.Speed * Time.deltaTime);
     }
 
     void ApplyVerticalMovement()
@@ -83,60 +73,7 @@ public class PlayerScript : MonoBehaviour
 
     void ApplyLookDirection( Vector3 direction )
     {
-        if (direction.sqrMagnitude > 0)
-        {
-//            float tmp = transform.position.y;
-//            mLookPos = transform.position + (direction.z * transform.forward) + (direction.x * transform.right);
-//            mLookPos.Set(mLookPos.x, tmp, mLookPos.z);
-
-            //Quaternion rotation = Quaternion.LookRotation(mLookPos - transform.position);
-            //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 6.0f);
-
-            
-        }
-    }
-
-    public void StickToWorldspace(Transform root, Transform camera, ref float directionOut)
-    {
-        Vector3 rootDirection = root.forward;
-
-        Vector3 cameraDirection = playerCamera.transform.forward;
-        Vector3 cameraRight = playerCamera.transform.right;
-        cameraRight.y = cameraDirection.y = 0;
-
-        Quaternion playerRot = Quaternion.FromToRotation(Vector3.Scale(cameraRight, direction), transform.forward);
-        Vector3 newPlayerLook = playerRot * Vector3.one;
-        //Debug.Log(newPlayerLook);
-
-        newPlayerLook = Vector3.Cross(Vector3.Scale(cameraRight, direction), Vector3.Scale(cameraDirection, direction));
-
-        Debug.DrawRay(new Vector3(root.position.x, root.position.y, root.position.z) + root.up * 2f, cameraRight, Color.red);
-        Debug.DrawRay(new Vector3(root.position.x, root.position.y, root.position.z) + root.up * 2f, Vector3.Scale(cameraRight, direction), Color.blue);
-        Debug.DrawRay(new Vector3(root.position.x, root.position.y, root.position.z) + root.up * 2f, Vector3.Scale(cameraDirection, direction), Color.green);
-
-        //Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.magenta);
-        Debug.DrawRay(new Vector3(root.position.x, root.position.y, root.position.z) + root.up * 2f, Vector3.Scale(Vector3.Scale(cameraRight, direction), Vector3.Scale(cameraDirection, direction)), Color.yellow);
-
-//        Quaternion referentialShift = Quaternion.FromToRotation(transform.up, cameraDirection);
-//        //Quaternion referentialShift = Quaternion.LookRotation(transform.forward, transform.up);
-//        Debug.Log(referentialShift);
-//        Vector3 moveDirection = referentialShift * direction;
-//        Vector3 axisSign = Vector3.Cross(moveDirection, rootDirection);
-//
-//        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), cameraDirection, Color.white);
-//        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), moveDirection, Color.green);
-//        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), axisSign, Color.red);
-//        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.magenta);
-//        Debug.DrawRay(new Vector3(root.position.x, root.position.y + 2f, root.position.z), direction, Color.blue);
-//
-//
-//
-//        float angleRootToMove = Vector3.Angle(rootDirection, moveDirection) * (axisSign.y >= 0 ? -1f : 1f);
-//
-//        angleRootToMove /= 180f;
-//        directionOut = angleRootToMove * turnSpeed;
-
-
+        transform.forward = direction;
     }
 
     #region UnityMethods
@@ -155,41 +92,35 @@ public class PlayerScript : MonoBehaviour
         playerCamera = Camera.main;
     }
 
+    //vector always pointing forward. Would have put it where it would make sense but c# doesnt allow that.
+    static Vector2 forwardVec = new Vector2(0, 1);
     //All input functions should be in Update, dont' be dumb and put them in fixed update
     void Update()
     {
-//        if (!Screen.lockCursor)
-//        {
-//            Screen.lockCursor = true;
-//        }
+        //find new direction for the player
+        Vector2 rawInput = MathEx.RangeToVector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector3 inputV3 = new Vector3(rawInput.x, 0, rawInput.y);
+        direction = playerCamera.transform.rotation * inputV3;
+        direction.y = 0;
+
+        //update the rotation and position
+        if(rawInput.x != 0 || rawInput.y != 0) {
+            ApplyLookDirection(direction);
+            ApplyHorizontalMovement(direction);
+        }
+
         if(Input.GetButtonDown("Fire1"))
             animator.Play("CombatAttackStart");
-        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         CheckJump();
-        var angle = 0.0f;
 
-        ApplyHorizontalMovement(direction);
-        ApplyLookDirection(direction);
         ApplyGravity();
         ApplyVerticalMovement();
-
-        StickToWorldspace(transform, playerCamera.transform, ref angle);
 
         if(animator)
         {
             animator.SetFloat("Speed", direction.sqrMagnitude);
             animator.SetFloat("Direction", direction.x, directionDampTime, Time.deltaTime);
         }
-    }
-
-    void FixedUpdate() 
-    {
-
-    }
-
-    void LateUpdate()
-    {
-        //cc.SimpleMove(Vector3.zero);
     }
     #endregion UnityMethods
 }
